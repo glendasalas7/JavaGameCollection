323b9bc70fb2b2d503d53fb67353b55eb313a991
Glenda <gsalas1@uco.edu>
2021-01-03 14:36:55 -0600

    test commit

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main{
     public static void main(String[] args){
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setTitle("Java Collection");
+        window.setTitle("Java Game Collection");
         window.setLocation(500, 200);
         window.setPreferredSize(new Dimension(600, 370));
         window.setResizable(false);
